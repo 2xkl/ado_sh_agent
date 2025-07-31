852dd43bad4e815d1384f6601ddd13d12e2375fb
2xkl <mjakstefan002@gmail.com>
2025-07-31 17:45:15 +0200

    Update pipe

--- terraform/build_agent/main.tf
+++ terraform/build_agent/main.tf
@@ -1,15 +1,15 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-#   tags     = var.tags
-}
-
-# module "rg" {
-#   source = "../modules/resource-group"
-
+# resource "azurerm_resource_group" "rg" {
 #   name     = var.resource_group_name
 #   location = var.location
+# #   tags     = var.tags
 # }
+
+module "rg" {
+  source = "../modules/resource-group"
+
+  name     = var.resource_group_name
+  location = var.location
+}
 
 # provider "azurerm" {
 #   features {}
