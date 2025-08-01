3e1218000d9d3a9f995e643b0e0bdba6f9d90d91
2xkl <mjakstefan002@gmail.com>
2025-08-01 16:09:20 +0200

    set some initial configuration

--- terraform/build_agent/variables.tf
+++ terraform/build_agent/variables.tf
@@ -10,39 +10,39 @@ variable "storage_account_name" {
   type = string
 }
 
-variable "container_name" {
-  type = string
-}
+# variable "container_name" {
+#   type = string
+# }
 
-variable "key_vault_name" {
-  type = string
-}
+# variable "key_vault_name" {
+#   type = string
+# }
 
 variable "user_managed_identity" {
   type = string
 }
 
-variable "deploytime" {
-  type = string
-}
+# variable "deploytime" {
+#   type = string
+# }
 
-variable "vm_config" {
-  type = list(object({
-    vmName            = string
-    vmHostname        = string
-    vmDataDisks       = list(object({ name = string }))
-    vmNicName         = string
-    vmPrivateIPAddress = string
-    vmNicSubnet       = string
-    vmAvailabilityZone = string
-  }))
-}
+# variable "vm_config" {
+#   type = list(object({
+#     vmName            = string
+#     vmHostname        = string
+#     vmDataDisks       = list(object({ name = string }))
+#     vmNicName         = string
+#     vmPrivateIPAddress = string
+#     vmNicSubnet       = string
+#     vmAvailabilityZone = string
+#   }))
+# }
 
-variable "admin_user" {
-  type = string
-}
+# variable "admin_user" {
+#   type = string
+# }
 
-variable "admin_password" {
-  type      = string
-  sensitive = true
-}
+# variable "admin_password" {
+#   type      = string
+#   sensitive = true
+# }
