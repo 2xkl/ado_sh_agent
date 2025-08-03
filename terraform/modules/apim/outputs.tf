a6f6bb64391df98da2a9e401d89add1c8743c9ac
2xkl <mjakstefan002@gmail.com>
2025-08-03 12:07:41 +0200

    Add logger

--- terraform/modules/apim/outputs.tf
+++ terraform/modules/apim/outputs.tf
@@ -1,10 +1,15 @@
 output "private_ip_address" {
-  # Jeśli APIM jest w VNet z trybem Internal, możemy zwrócić prywatne IP z interfejsów
-  value       = azurerm_api_management.apim.private_ip_addresses[0]
-  description = "Private IP address of the APIM inside the VNet"
+  value = azurerm_api_management.apim.private_ip_addresses[0]
 }
 
 output "apim_service_url" {
-  value       = azurerm_api_management.apim.gateway_url
-  description = "APIM Gateway URL"
+  value = azurerm_api_management.apim.gateway_url
+}
+
+output "apim_name" {
+  value = azurerm_api_management.apim.name
+}
+
+output "apim_id" {
+  value = azurerm_api_management.apim.id
 }
