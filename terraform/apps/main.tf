d9921169642feff1a3f0449bd817214dfe54223b
2xkl <mjakstefan002@gmail.com>
2025-08-04 20:25:44 +0200

    sb roles

--- terraform/apps/main.tf
+++ terraform/apps/main.tf
@@ -135,7 +135,7 @@ module "publisher" {
   location                   = var.location
   resource_group_name        = module.rg.name
   oidc_issuer_url            = data.azurerm_kubernetes_cluster.aks.oidc_issuer_url
-  servicebus_subscription_id = module.servicebus.subscription_id
+  servicebus_subscription_id = module.servicebus.topic_id
 }
 
 module "receiver" {
